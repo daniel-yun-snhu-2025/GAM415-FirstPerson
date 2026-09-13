796f489153e404d338faf0a6677a8f3960465609
Daniel Yun <daniel.yun@snhu.edu>
2026-09-13 01:13:16 -0500

    Initial commit: First Person C++ project (UE 5.8.2) for GAM 415

---
+++ Source/firstperson415/firstperson415.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "firstperson415.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, firstperson415, "firstperson415" );
+
+DEFINE_LOG_CATEGORY(Logfirstperson415)
---
+++ Source/firstperson415/firstperson415GameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "firstperson415GameMode.h"
+
+Afirstperson415GameMode::Afirstperson415GameMode()
+{
+	// stub
+}
